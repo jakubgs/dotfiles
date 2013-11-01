8b58af1c04ddeb6edeee8fb2311a72b458a61bbe
Jakub Sokołowski <panswiata@gmail.com>
2013-11-01 23:10:02 +0100

    lowered the volume change when scrolling, made left mouse key do nothing

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -185,10 +185,10 @@ myvolume = wibox.widget.textbox()
 vicious.register(myvolume, vicious.widgets.volume, "| Vol: $1% |", 1, "PCM")
 
 myvolume:buttons(awful.util.table.join(
-awful.button({ }, 1, function () awful.util.spawn(homedir .. "bin/mute", false) end),
-awful.button({ }, 3, function () awful.util.spawn(terminal .. " -e alsamixer", true) end),
-awful.button({ }, 4, function () awful.util.spawn("amixer -q set PCM 2dB+", false) end),
-awful.button({ }, 5, function () awful.util.spawn("amixer -q set PCM 2dB-", false) end)
+awful.button({ }, 2, function () awful.util.spawn(homedir .. "bin/mute", false) end),
+awful.button({ }, 3, function () awful.util.spawn("volti-mixer", true) end),
+awful.button({ }, 4, function () awful.util.spawn("amixer -q set PCM 1dB+", false) end),
+awful.button({ }, 5, function () awful.util.spawn("amixer -q set PCM 1dB-", false) end)
 ))
 
 -- Create a wibox for each screen and add it
