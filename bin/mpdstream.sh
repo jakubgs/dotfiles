c766501a0cb0a9c3d88931d2b35c19a316d19343
PonderingGrower <panswiata@gmail.com>
2015-02-10 10:14:13 +0100

    added more checks, moved command to a variable

--- bin/mpdstream.sh
+++ bin/mpdstream.sh
@@ -3,16 +3,23 @@
 # continously wait for a stream
 
 STREAM_URL="http://nerv.no-ip.org:8000/mpd.ogg"
+MPV_OPTS="--no-cache --cache-pause --really-quiet --loop=no --stream-lavf-o=timeout=1"
 
 while true ; do
     # check if a song is playing
     if mpc | grep playing ; then
         echo "Stream is playing!"
         # if so connect to the stream
-        mpv --no-cache --quiet $STREAM_URL
+        mpv $MPV_OPTS $STREAM_URL
+        echo "Stream stopped!"
+    fi
+    if mpc | grep playing ; then
+        continue
     fi
     echo "Waiting for mpd state change..."
     # wait for an event
-    mpc idle player
+    while ! timeout 10 mpc idle player; do
+        continue
+    done    
     echo "Stream state changed..."
 done
