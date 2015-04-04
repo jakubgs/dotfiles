e274c09d750982fed114d832b602ddacc8aebfc6
PonderingGrower <panswiata@gmail.com>
2015-04-05 00:00:34 +0200

    added logging

--- bin/backup.py
+++ bin/backup.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python
-import os, sys, time, re
+import os, sys, time, re, logging
 try:
     import sh
 except ImportException:
-    print 'Failed to import module "sh". Please install it.'
+    log.info('Failed to import module "sh". Please install it.')
+
+class ContextFilter(logging.Filter):
+    def filter(self, record):
+        record.c_host = host
+        return True
+
+def setup_logging(log_file):
+    FORMAT = '%(asctime)s - %(c_host)-17s - %(levelname)s: %(message)s'
+    logging.basicConfig(format=FORMAT)
+
+    log = logging.getLogger('backup')
+    log.addFilter(ContextFilter())
+    log.setLevel(logging.DEBUG)
+
+    fhandler = logging.FileHandler(log_file)
+    fhandler.setFormatter(logging.Formatter(FORMAT))
+    log.addHandler(fhandler)
+    return log
 
 def show_output(line):
-    print line
+    log.info(line)
 
 def check_ping(host):
     r = ping()
@@ -14,8 +32,11 @@ def check_ping(host):
     time = float(m.group(1))
     return time
 
+log_file='/var/log/backup.log'
+log = setup_logging(log_file)
+
+host = ''
 minimal_ping = 6
-log_file = '/var/log/backup.log'
 
 assets = [
     '/home/jso'
@@ -23,7 +44,7 @@ assets = [
 targets = [
     { 'user': 'jacob',  'host': 'enpoka',
         'dir': '/home/jacob/backup/' },
-    { 'user': 'sochan', 'host': 'nerv.no-ip.org',
+    { 'user': 'sochan', 'host': 'melchior',
         'dir': '/mnt/raid1/backup/homes/' }
 ]
 
@@ -35,21 +56,21 @@ for target in targets:
     dest = "{}@{}:{}".format(target['user'], target['host'], target['dir'])
     host = '{}@{}'.format(target['user'], target['host'])
 
-    ssh = sh.ssh.bake('-q', host)
+    ssh = sh.ssh.bake('-q', '-o ConnectTimeout=2', host)
     ping = sh.ping.bake(target['host'], '-c1')
     try:
         ssh.exit()
     except Exception as e:
-        print 'Host not available: {}'.format(host)
+        log.info('Host not available')
         continue
 
     ping = check_ping(host) 
     if ping > minimal_ping:
-        print 'Ping or host too slow({}s), abandoning: {}'.format(ping, host)
+        log.warning('Ping or host too slow({}s), abandoning.'.format(ping))
         continue
 
     for asset in assets:
-        print "rsync: {} -> {}".format(asset, dest)
+        log.info("rsync: {} -> {}".format(asset, target['dir']))
         rsync = sh.rsync.bake('-arut', '--delete', rsync_opts,
                               '--exclude=.*', '--exclude=.*/', 
                               asset, dest, _out=show_output)
@@ -58,7 +79,7 @@ for target in targets:
             rsync_output = rsync()
             end = time.time()
         except Exception as e:
-            print 'Failed to execute command: {}'.format(rsync)
-            print 'Output: {}'.format(rsync_output)
+            log.error('Failed to execute command: {}'.format(rsync))
+            log.error('Output: {}'.format(host, rsync_output))
             continue
-        print 'Finished in: {}'.format(end - start)
+        log.info('Finished in: {}'.format(end - start))
