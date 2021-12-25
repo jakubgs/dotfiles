a4b757552456ef516a9b4b7723509dbf8dc0f3fe
Jakub Sokołowski <jakub@gsokolowski.pl>
2021-12-25 15:33:28 +0100

    zfssecmount.sh: fix xargs args to work with busybox
    Signed-off-by: Jakub Sokołowski <jakub@gsokolowski.pl>

--- bin/zfssecmount.sh
+++ bin/zfssecmount.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Load password for decrypting ZFS secret volumes and mount.
 sudo zfs load-key rpool/secret
-awk '/noauto/{print $2}' /etc/fstab | xargs -l sudo mount
+awk '/noauto/{print $2}' /etc/fstab | xargs -n1 sudo mount
 sudo systemctl restart syncthing gitweb
